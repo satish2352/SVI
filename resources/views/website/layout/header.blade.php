78dcd21f5e5058b220e02f358ab48f49c67df8e7
Savita444 <savita.hajare@sumagoinfotech.com>
2024-02-09 17:53:55 +0530

    change env

--- resources/views/website/layout/header.blade.php
+++ resources/views/website/layout/header.blade.php
@@ -8,7 +8,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
   <link href="{{ asset('website/assets/scss/home.css') }}" rel="stylesheet">
-  <link href="{{ asset('website/assets/scss/common.scss') }}" rel="stylesheet">
+  <link href="{{ asset('website/assets/scss/common.css') }}" rel="stylesheet">
   <link href="{{ asset('website/assets/scss/about.scss') }}" rel="stylesheet">
   <link href="{{ asset('website/assets/scss/contact.scss') }}" rel="stylesheet">
   <link href="{{ asset('website/assets/scss/media.scss') }}" rel="stylesheet">
