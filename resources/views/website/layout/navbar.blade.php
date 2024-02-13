3bad0de57a474cb0bd8b92f27f74aff30186eb1f
Shubham Kothavade <shubham.kothavade09@gmail.com>
2024-02-13 14:57:02 +0530

    changes

--- resources/views/website/layout/navbar.blade.php
+++ resources/views/website/layout/navbar.blade.php
@@ -21,9 +21,19 @@
         <li class="nav-item">
           <a class="nav-link px-md-3" href="{{ route('aboutus') }}">About Us</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link px-md-3" href="{{ route('product') }}">Product & Services</a>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Product & Services
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="{{ route('product') }}">Product</a></li>
+            <li class="px-3"><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="{{ route('services') }}">Services</a></li>
+          </ul>
         </li>
+        <!-- <li class="nav-item">
+          <a class="nav-link px-md-3" href="{{ route('product') }}">Product & Services</a>
+        </li> -->
         <li class="nav-item">
           <a class="nav-link px-md-3" href="{{ route('media') }}">Media</a>
         </li>
@@ -45,15 +55,15 @@
 <!-- ------------------------------------------------------------------------------------>
 
 <section>
-  <div class="container-fluid p-0 bg-white">
+  <div class="container-fluid p-0 bg-white" style="line-height:0;">
     <video autoplay loop muted playsinline class="videoBanner">
       <source src="{{ asset('website/assets/images/home/sampleVideo.mp4')}}" type="video/mp4">
-      <!-- Add additional source elements for different formats if needed -->
       Your browser does not support the video tag.
     </video>
   </div>
   <div class="onVideoBanner">
-    <img src="{{ asset('website/assets/images/home/onVideoBanner.png')}}" class="w-75 float-end" alt="onVideoBanner.png">
+    <img src="{{ asset('website/assets/images/home/onVideoBanner.png')}}" class=" float-end d-none d-md-block" alt="onVideoBanner.png">
+    <img src="{{ asset('website/assets/images/home/onVideoMobBannerText.png')}}" class=" float-end d-block d-md-none" alt="onVideoBanner.png">
   </div>
 </section>
 
