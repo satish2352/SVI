19dc2de83048fa7f81fd2a2c66a122cc9014e1bc
Savita444 <savita.hajare@sumagoinfotech.com>
2024-02-16 15:35:30 +0530

    change in navbar

--- resources/views/website/layout/navbar.blade.php
+++ resources/views/website/layout/navbar.blade.php
@@ -21,7 +21,7 @@ if (strpos($_SERVER['REQUEST_URI'], 'aboutus') !== false) {
 $common_data = App\Http\Controllers\Website\IndexController::getCommonBanner($pageName);
 ?>
 
-<nav class="navbar navbar-expand-lg bg-white">
+{{-- <nav class="navbar navbar-expand-lg bg-white">
   <div class="container-fluid">
     <a class="navbar-brand navBarBrand d-md-none d-lg-none" href="#"><img class="navbarLogo"
               src="{{ asset('website/assets/images/home/mobLogo.png')}}" class="logo" alt="logo" /></a>
@@ -63,7 +63,54 @@ $common_data = App\Http\Controllers\Website\IndexController::getCommonBanner($pa
       </ul>
     </div>
   </div>
+</nav> --}}
+<nav class="navbar navbar-expand-lg bg-white">
+  <div class="container-fluid">
+    <a class="navbar-brand navBarBrand d-md-none d-lg-none" href="#"><img class="navbarLogo"
+      src="{{ asset('website/assets/images/home/mobLogo.png')}}" class="logo" alt="logo" /></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+      aria-controls="offcanvasNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+          <a class="navbar-brand navBarBrand d-md-none d-lg-none" href="#"><img class="navbarLogo"
+            src="{{ asset('website/assets/images/home/mobLogo.png')}}" class="logo" alt="logo" /></a>
+
+        </h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav mx-md-auto navbarView">
+          <li class="nav-item d-none d-md-block">
+            <a class="navbar-brand" href="{{url('/')}}"> <img class="navbarLogo"
+                src="{{ asset('website/assets/images/logo.png')}}" class="logo" alt="logo" /></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php if($pageName === 'Home') echo 'active'; ?>" aria-current="page" href="{{url('/')}}">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php if($pageName === 'About') echo 'active'; ?>" href="{{ route('aboutus') }}">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php if($pageName === 'Product') echo 'active'; ?>" aria-current="page" href="{{ route('product') }}">Product</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php if($pageName === 'Services') echo 'active'; ?>" href="{{ route('services') }}">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php if($pageName === 'Media') echo 'active'; ?>" aria-current="page" href="{{ route('media') }}">Media</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link headerBtn <?php if($pageName === 'Contact') echo 'active'; ?>" href="{{ route('contactus') }}">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </nav>
+
 <section>
   <div class="container-fluid p-0 bg-white">
     @foreach ($common_data['website_banner_data'] as $item)
