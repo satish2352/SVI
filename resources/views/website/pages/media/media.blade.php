628da4271cb777a7b47d4a1603e5c4c8c7b4072c
Savita444 <savita.hajare@sumagoinfotech.com>
2024-02-16 11:15:12 +0530

    changes in media

--- resources/views/website/pages/media/media.blade.php
+++ resources/views/website/pages/media/media.blade.php
@@ -56,7 +56,7 @@
               @foreach ($data_output as $media)
 
               <div class="col-lg-3 col-md-3 col-sm-8">
-                <img src="{{ Config::get('DocumentConstant.MEDIA_VIEW') }}{{ $media['image'] }}" class="w-100" alt="media1" />
+                <img src="{{ Config::get('DocumentConstant.MEDIA_VIEW') }}{{ $media['image'] }}" class="w-100" alt="media1" style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;" />
               </div>
               @endforeach
               @endif
